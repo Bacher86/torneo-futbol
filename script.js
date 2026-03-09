5bde9f0926b2e386e929930d07654fbf92d2d544
Bacher86 <uriel.bacher@gmail.com>
2026-03-09 14:24:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,24 @@ function doGet() {
 }
 
 function doPost(e) {
-  var listaAsistencia = JSON.parse(e.postData.contents);
+  var pedido = JSON.parse(e.postData.contents);
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var values = sheet.getDataRange().getValues();
   
-  listaAsistencia.forEach(asistente => {
-    for (var i = 1; i < values.length; i++) {
-      if (values[i][0] === asistente.nombre) {
-        sheet.getRange(i + 1, 2).setValue(values[i][1] + parseInt(asistente.puntos)); // Suma puntos
-        sheet.getRange(i + 1, 3).setValue(values[i][2] + 1); // Suma 1 partido jugado
-        sheet.getRange(i + 1, 4).setValue(values[i][3] + parseInt(asistente.goles)); // Suma goles
+  if (pedido.accion === "nuevo") {
+    // Agrega una fila nueva al final con 0 puntos
+    sheet.appendRow([pedido.nombre, 0, 0, 0]);
+  } 
+  else if (pedido.accion === "partido") {
+    var values = sheet.getDataRange().getValues();
+    pedido.datos.forEach(asistente => {
+      for (var i = 1; i < values.length; i++) {
+        if (values[i][0] === asistente.nombre) {
+          sheet.getRange(i + 1, 2).setValue(values[i][1] + parseInt(asistente.puntos));
+          sheet.getRange(i + 1, 3).setValue(values[i][2] + 1);
+          sheet.getRange(i + 1, 4).setValue(values[i][3] + parseInt(asistente.goles));
+        }
       }
-    }
-  });
-  return ContentService.createTextOutput("Datos guardados").setMimeType(ContentService.MimeType.TEXT);
+    });
+  }
+  return ContentService.createTextOutput("Ok");
 }
